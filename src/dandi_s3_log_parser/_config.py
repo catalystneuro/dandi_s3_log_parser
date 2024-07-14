5b366bef9988ba957b65e6771a4a4c75412d1ea6
CodyCBakerPhD <codycbakerphd@gmail.com>
2024-07-14 13:49:34 -0400

    add function for calculating salt

--- src/dandi_s3_log_parser/_config.py
+++ src/dandi_s3_log_parser/_config.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import hashlib
 
 REQUEST_TYPES = ("GET", "PUT", "HEAD")
 
@@ -11,3 +12,24 @@ _IP_ADDRESS_TO_REGION_FILE_PATH = DANDI_S3_LOG_PARSER_BASE_FOLDER_PATH / "ip_add
 if "IPINFO_CREDENTIALS" not in os.environ:
     raise ValueError("The environment variable 'IPINFO_CREDENTIALS' must be set to import `dandi_s3_log_parser`!")
 IPINFO_CREDENTIALS = os.environ["IPINFO_CREDENTIALS"]
+
+
+def get_hash_salt(base_raw_s3_log_folder_path: str | pathlib.Path) -> bytes:
+    """
+    Calculate the salt (in bytes) used for IP hashing.
+
+    Uses actual data from the first line of the first log file in the raw S3 log folder, which only we have access to.
+
+    Otherwise, it would be fairly easy to iterate over every possible IP address and find the SHA1 of it.
+    """
+    base_raw_s3_log_folder_path = pathlib.Path(base_raw_s3_log_folder_path)
+
+    # Retrieve the first line of the first log file (which only we know) and use that as a secure salt
+    first_log_file_path = base_raw_s3_log_folder_path / "2019" / "10" / "01.log"
+
+    with open(file=first_log_file_path, mode="r") as io:
+        first_line = io.readline()
+
+    hash_salt = hashlib.sha1(string=bytes(first_line, "utf-8"))
+
+    return hash_salt.digest()
