c7e6c1218830733b3e79249f48ca260b34f906eb
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-08-16 15:54:45 -0400

    Add Zarr tests (#51)
    * add and debug zarr example in base test
    
    * add to mapper tests too
    
    ---------
    
    Co-authored-by: CodyCBakerPhD <codycbakerphd@gmail.com>

--- tests/test_reduce_dandi_raw_s3_log.py
+++ tests/test_reduce_dandi_raw_s3_log.py
@@ -33,23 +33,28 @@ def test_reduce_dandi_raw_s3_log_example_0(tmpdir: py.path.local) -> None:
     assert number_of_output_files != 0, f"Test expected_output folder ({test_reduced_s3_logs_folder_path}) is empty!"
 
     # Increment this over time as more examples are added
-    expected_number_of_output_files = 2
+    expected_number_of_output_files = 3
     assert (
         number_of_output_files == expected_number_of_output_files
     ), f"The number of asset files ({number_of_output_files}) does not match expectation!"
 
     expected_asset_ids = [path.stem for path in expected_reduced_s3_logs_folder_path.rglob("*.tsv")]
-    for test_parsed_s3_log_file_path in test_output_file_paths:
+    for test_reduced_s3_log_file_path in test_output_file_paths:
         assert (
-            test_parsed_s3_log_file_path.stem in expected_asset_ids
-        ), f"Asset ID {test_parsed_s3_log_file_path.stem} not found in expected asset IDs!"
+            test_reduced_s3_log_file_path.stem in expected_asset_ids
+        ), f"Asset ID {test_reduced_s3_log_file_path.stem} not found in expected asset IDs!"
 
-        test_parsed_s3_log = pandas.read_table(filepath_or_buffer=test_parsed_s3_log_file_path)
+        is_asset_zarr = "zarr" in str(test_reduced_s3_log_file_path)
+        if is_asset_zarr:
+            blob_id = test_reduced_s3_log_file_path.stem
+            expected_parsed_s3_log_file_path = expected_reduced_s3_logs_folder_path / "zarr" / f"{blob_id}.tsv"
+        else:
+            blob_id = test_reduced_s3_log_file_path.stem
+            expected_parsed_s3_log_file_path = (
+                expected_reduced_s3_logs_folder_path / "blobs" / blob_id[:3] / blob_id[3:6] / f"{blob_id}.tsv"
+            )
 
-        blob_id = test_parsed_s3_log_file_path.stem
-        expected_parsed_s3_log_file_path = (
-            expected_reduced_s3_logs_folder_path / "blobs" / blob_id[:3] / blob_id[3:6] / f"{blob_id}.tsv"
-        )
+        test_reduced_s3_log = pandas.read_table(filepath_or_buffer=test_reduced_s3_log_file_path)
         expected_parsed_s3_log = pandas.read_table(filepath_or_buffer=expected_parsed_s3_log_file_path)
 
-        pandas.testing.assert_frame_equal(left=test_parsed_s3_log, right=expected_parsed_s3_log)
+        pandas.testing.assert_frame_equal(left=test_reduced_s3_log, right=expected_parsed_s3_log)
